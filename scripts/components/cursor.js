5e466e13d1551d9e328d13e21fb5687e44ac5f46
Victor Potasso <victorpotasso@gmail.com>
2019-04-14 10:08:28 +1200

    fixed style for mobile

--- scripts/components/cursor.js
+++ scripts/components/cursor.js
@@ -1,5 +1,3 @@
-import TweenMax, { Power0 }  from 'https://unpkg.com/gsap@2.1.2/TweenMax.js';
-
 export default class Cursor extends HTMLElement {
   constructor() {
     super();
@@ -13,10 +11,14 @@ export default class Cursor extends HTMLElement {
   onMouseMove({ target, clientX, clientY }) {
     const x = clientX - (this.clientWidth >> 1);
     const y = clientY - (this.clientHeight >> 1);
-    TweenMax.to(this, 0.15, { x, y, ease: Power0.easeInOut });
+    this.style.left = `${x}px`;
+    this.style.top = `${y}px`;
 
     const action = target.dataset.cursorAction;
-    TweenMax.to(this, 0.35, { scale: action == 'highlight' ? 1.5 : 1, ease: Power0.easeInOut });
+    if (action == 'highlight')
+      this.classList.add('highlight');
+    else
+      this.classList.remove('highlight');
 
     this.classList.remove('hidden');
   }
